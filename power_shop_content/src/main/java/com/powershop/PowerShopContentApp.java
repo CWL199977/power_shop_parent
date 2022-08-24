16d36497076bacc87ba7223bf04903650a32e2db
chang <cwlcwl.@gmail.com>
2022-08-24 09:20:30 +0800

    新的提交

---
+++ power_shop_content/src/main/java/com/powershop/PowerShopContentApp.java
@@ -0,0 +1,15 @@
+package com.powershop;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.powershop.mapper")
+public class PowerShopContentApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PowerShopContentApp.class, args);
+    }
+}
